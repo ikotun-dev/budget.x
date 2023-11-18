9f6ef029df13f6eb58dafcbd6bb7206d9b8adc51
ikotun-dev <danlogan2003@gmail.com>
2023-11-18 15:43:15 +0100

    added features

--- BudgetX/core/models.py
+++ BudgetX/core/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Family(models.Model) : 
+    id = models.IntegerField(primary_key=True)
+    name = models.CharField(max_length=255)
+    username = models.CharField(max_length=255)
+    email = models.CharField(max_length=255)
+    family_no = models.IntegerField()
+
+class Expense(models.Model)  : 
+    id = models.IntegerField(primary_key=True)
+    amount = models.IntegerField()

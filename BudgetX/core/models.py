0dd92a966d8c02b868f980ccbba2549813223a0b
ikotun-dev <danlogan2003@gmail.com>
2023-11-18 23:45:20 +0100

    bypass session

--- BudgetX/core/models.py
+++ BudgetX/core/models.py
@@ -2,11 +2,12 @@ from django.db import models
 
 # Create your models here.
 class Family(models.Model) : 
-    id = models.IntegerField(primary_key=True)
+    id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=255)
     username = models.CharField(max_length=255)
     email = models.CharField(max_length=255)
     family_no = models.IntegerField()
+    password = models.CharField(max_length=255)
 
     class Meta:
         db_table = 'family'
--- BudgetX/core/views.py
+++ BudgetX/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from . import models
 
 # Create your views here.
@@ -8,20 +8,58 @@ def index(request) :
         password = request.POST.get('password')
         check_family = models.Family.objects.filter(username=username)
         if check_family.exists() : 
+           request.session['username'] = username
+           print("user exists")
+           return redirect('add-expense')
+        else : 
             context = { 
                 "error" : "user does not exist"
             }
-            return render (request, context)
-        else : 
+            return render (request, 'index.html', context)
             print("does not exists")
 
     return render(request, 'index.html')
 
 def signup(request) : 
+    if request.method == "POST" :
+        family_name = request.POST.get('family_name')
+        no_of_family = request.POST.get('no_of_family')   
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        email = request.POST.get('email')
+
+        check_family = models.Family.objects.filter(username=username)
+        if check_family.exists() : 
+            context = { 
+                "error" : "family username already exist"
+            }
+            return render (request, 'signup.html', context)
+
+        check_family_email = models.Family.objects.filter(email=email)
+        if check_family.exists() : 
+            context = { 
+                "error" : "family email already exist"
+            }
+            return render (request, 'signup.html', context)
+        else : 
+            context = { 
+                "sucess" : "successfully created, please Login"
+            }
+           
+            new_family = models.Family(name=family_name, family_no=no_of_family, username=username, password=password, email=email)
+            new_family.save()
+            print("family created succesffully")
+            return render (request, 'signup.html', context)
+
+
     return render(request, 'signup.html')
 
 def add_expense(request) : 
+    username = request.session.get('username')
+    print("im now in expense page")
+    print("logged in user is : " + " " + username)
     return render(request, 'add_expense.html')
 
 def add_budget(request) : 
+
     return render(request, 'add_budget.html')
